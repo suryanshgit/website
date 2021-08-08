918c6e40360fc4084973c7e5c6e99043259b9364
suryanshgit <79361173+suryanshgit@users.noreply.github.com>
2021-08-08 18:39:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ const RouteMenu=(login)=>{
   return(
     <>
   <Switch>
+    <Route exact path="/website" component={Home}/>
     <Route exact path="/" component={Home}/>
     <Route exact path="/home" component={Home}/>
     <Route exact path="/about" component={About}/>
